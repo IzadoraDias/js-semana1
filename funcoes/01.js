d925540fdae545262e405aa78e71385537b04b0e
izadoradias <izadoradias@gmail.com>
2018-09-13 16:50:04 -0300

    resolução exerc. funções

--- funcoes/01.js
+++ funcoes/01.js
@@ -1,11 +1,25 @@
-// hello("what") ->
-// hello() ->
+// hello("what") -> não acontece nada, pq o what que é um elemento não está sendo chamado na  function
+// hello() -> "Hello, world!"
 function hello() {
 	return "Hello, world!"
 }
 
-// show(4) ->
-// show(-4) ->
+// show(4) ->  
+ function show(x) {
+	console.log(`${x} ${x * x}`) //substitui o x por 4 mostra os resultados= 4 16
+	return x * x      //retorna o resultado = 16          
+ 	console.log(`${x} ${x * x * x}`)
+	return x * x * x
+}
+                              
+// show(-4) ->  
+//function show(x) {
+//	console.log(`${x} ${x * x}`)
+//	return x * x
+//	console.log(`${x} ${x * x * x}`)
+//	return x * x * x
+//}
+
 function show(x) {
 	console.log(`${x} ${x * x}`)
 	return x * x
@@ -13,11 +27,11 @@ function show(x) {
 	return x * x * x
 }
 
-// eq3() ->
-// eq3(1, 2, 3) ->
-// eq3("1", 1, 3) ->
-// eq3("!", "a", "b") ->
-// eq3("!", "!", !) ->
+// eq3() -> 1
+// eq3(1, 2, 3) -> 0
+// eq3("1", 1, 3) -> 0
+// eq3("!", "a", "b") -> 0
+// eq3("!", "!", !) -> error 
 function eq3(a, b, c) {
 	if (a === b && a === c) {
 		return 1
@@ -26,20 +40,28 @@ function eq3(a, b, c) {
 	}
 }
 
-// Resultado:
+// Resultado: return 9, 11
 function sum(x) {
 	return x + 2
 }
 const x = 5
 console.log(`${sum(x + 2)}, ${sum(sum(x + 2))}`)
+//
 
-// Resultado:
-function confusion(x, y) {
-	x = 2 * x + y
-	return x
+// Resultado: 26, 12
+function confusion(a, b) {
+	a = 2 * a + b
+	return a
 }
 let x = 2
 let y = 5
-y = confusion(y, x)
-x = confusion(y, x)
+// confusion(5, 2)
+// x = 2 * 5 + 2 
+// x = 12
+y = confusion(y, x) // 12
+// confusion(12, 2)
+// x = 2 * 12 + 2
+// x = 26
+x = confusion(y, x) // 26
 console.log(`${x}, ${y}`)
+
--- funcoes/02.js
+++ funcoes/02.js
@@ -6,6 +6,10 @@
 // <objeto>. <nome> decidiu levar
 // <objeto> pra casa."
 
+function frase(nome, pronome, objeto){
+     return `${nome} foi caminhando parque. ${pronome} encontrou ${objeto}. decidiu levar
+     ${objeto} pra casa.`
+}
 
 
 
@@ -13,6 +17,10 @@
 // em Javascript que recebe
 // um número qualquer e retorna
 // a raiz quadrada dele.
+
+function raizquadrada(){
+    return Math.sqrt()
+}
 
 
 
@@ -22,21 +30,28 @@
 // número qualquer e retorna
 // a representação binaria dele.
 
-
+function conversorbinario(x){
+    return x.toString(x)
+}
 
 
 // Escreva uma função em Javascript
 // que recebe uma lista de números
 // e retorna o maior número da lista.
-
-
+ 
+function maiornumero(arr){
+    return Math.max(...arr)
+}
 
 
 // Escreva uma função em Javascript
 // que vai checar se duas ou mais strings
 // sao anagramas umas das outras.
 
-
+function comparation(x, y){
+   return (x, y).split("").sort().join("")        
+    }
+        
 
 
 // Escreva uma função em Javascript
